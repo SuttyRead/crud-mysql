15545486163ce6525856d75769fe36ee1490508a
“Roman <roman.yaremenko@nixsolutions.com>
2019-02-19 17:40:13 +0200

    add logging

--- src/main/java/com/ua/sutty/mysql/controller/UserController.java
+++ src/main/java/com/ua/sutty/mysql/controller/UserController.java
@@ -5,6 +5,7 @@ import com.ua.sutty.mysql.model.User;
 import com.ua.sutty.mysql.repository.UserRepository;
 import com.ua.sutty.mysql.validator.EmailValidator;
 import com.ua.sutty.mysql.validator.UsernameValidator;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.WebDataBinder;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Slf4j
 @RestController
 public class UserController {
 
@@ -38,9 +40,13 @@ public class UserController {
 
     @GetMapping("/users")
     public ResponseEntity<List<User>> getAllUsers() {
+
+        log.trace("Call getAllUsers method");
+
         List<User> users = this.userRepository.findAll();
 
         if (users.isEmpty()) {
+            log.trace("User is empty");
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
 
@@ -49,9 +55,13 @@ public class UserController {
 
     @GetMapping("/users/{id}")
     public ResponseEntity<User> getUser(@PathVariable("id") final Long id) {
+
+        log.trace("Call getUser method");
+
         User user = userRepository.findUserById(id);
 
         if (user == null) {
+            log.trace("User is null");
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
@@ -60,9 +70,14 @@ public class UserController {
 
     @PostMapping("/users")
     public ResponseEntity<User> saveUser(@RequestBody @Valid final UserForm userForm) {
+
+        log.trace("Call saveUser method");
+
         if (userForm == null) {
+            log.trace("UserForm is null");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
+
         User user = userForm.toUser();
         this.userRepository.save(user);
 
@@ -72,17 +87,26 @@ public class UserController {
     @PutMapping("/users/{id}")
     public ResponseEntity<User> updateUser(@RequestBody @Valid final User user,
                                            @PathVariable final Long id) {
+        log.trace("Call updateUser method");
+
         if (user == null) {
+            log.trace("User is null");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
+
         if (!userRepository.existsById(id)) {
+            log.trace("User doesn't exists");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
+
         User userById = userRepository.findUserById(id);
+
         if (!userById.getUsername().equals(user.getUsername())
                 || !userById.getEmail().equals(user.getEmail())) {
+            log.trace("You can't change username or email");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
+
         user.setId(id);
         this.userRepository.save(user);
 
@@ -91,9 +115,13 @@ public class UserController {
 
     @DeleteMapping("/users/{id}")
     public ResponseEntity<User> deleteUser(@PathVariable("id") final Long id) {
+
+        log.trace("Call deleteUser method");
+
         User user = this.userRepository.findUserById(id);
 
         if (user == null) {
+            log.trace("User is null");
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
